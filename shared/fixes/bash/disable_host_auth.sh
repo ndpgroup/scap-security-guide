07b7d55b6749fb11d0d083699f18ac224419f546
James E. Flemer <james.flemer@ndpgroup.com>
2018-05-04 05:23:24 +0000

    Explicitly set sshd_config values (for DISA Benchmark)

--- shared/fixes/bash/disable_host_auth.sh
+++ shared/fixes/bash/disable_host_auth.sh
@@ -1,6 +1,4 @@
 # platform = multi_platform_rhel, multi_platform_fedora
-grep -q ^HostbasedAuthentication /etc/ssh/sshd_config && \
-  sed -i "s/HostbasedAuthentication.*/HostbasedAuthentication no/g" /etc/ssh/sshd_config
-if ! [ $? -eq 0 ]; then
-    echo "HostbasedAuthentication no" >> /etc/ssh/sshd_config
-fi
+. /usr/share/scap-security-guide/remediation_functions
+
+replace_or_append '/etc/ssh/sshd_config' '^HostbasedAuthentication' 'no' '@CCENUM@' '%s %s'
